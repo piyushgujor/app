34597a7ea3b0005a4e2470ede6a1346740ff8863
Piyush Gujor <piyushgujor678@gmail.com>
2026-03-19 15:18:52 +0530

    Add files via upload

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,14 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        if (args.length == 0) {
+            System.out.println("Hello World");
+        } else {
+            System.out.print("Hello ");
+            for (int i = 0; i < args.length; i++) {
+                System.out.print(args[i] + " ");
+            }
+            System.out.println();
+        }
     }
 }
