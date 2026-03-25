13836eee65548f5532de92b326fc5abf27fcb3e4
Piyush Gujor <piyushgujor678@gmail.com>
2026-03-25 13:22:37 +0530

    Create HelloWorld6.java

---
+++ HelloWorld6.java
@@ -0,0 +1,22 @@
+public class HelloApp {
+    public static void main(String[] args) {
+
+        // No arguments case
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+        } else {
+            String result = "";
+
+            // Enhanced for loop
+            for (String name : args) {
+                result += name + ", ";
+            }
+
+            // Remove trailing ", " using substring
+            result = result.substring(0, result.length() - 2);
+
+            // Final output
+            System.out.println("Hello, " + result + "!");
+        }
+    }
+}
